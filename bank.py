873fde51ffe7b551986fadc311ae5723cb5c76ae
Nursultan Zhanybekov <121675952+arnursultan@users.noreply.github.com>
2023-03-18 19:44:20 +0600

    done

--- bank.py
+++ bank.py
@@ -1,10 +1,10 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.uic import loadUi
+import sys
 import sqlite3
 import time
-import sys
 
-class StartDB():
+class StartDB:
     def __init__(self):
         self.connect = sqlite3.connect('bank.db')
         self.connect.execute("""
@@ -13,11 +13,16 @@ class StartDB():
             password VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL UNIQUE,
             created VARCHAR(100),
-            balance INT
-        );
+            balance INT, 
+            utilities_electricity INTEGER DEFAULT 34000,
+            utilities_water INTEGER DEFAULT 34000, 
+            utilities_sewer INTEGER DEFAULT 34000, 
+            utilities_gas INTEGER DEFAULT 34000, 
+            utilities_garbage INTEGER DEFAULT 34000
+            );
         """)
         self.connect.commit()
-        
+
 class SignUp(QWidget):
     def __init__(self):
         super(SignUp, self).__init__()
@@ -35,26 +40,28 @@ class SignUp(QWidget):
     def register(self):
         login = self.login.text()
         password = self.password.text()
-        email = self.email.text()
+        mail = self.mail.text()
         self.show_error()
         cursor = self.db.connect.cursor()
         try:
-            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{email}', '{time.ctime()}', 0);")
+            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{mail}', '{time.ctime()}', 0, 34000, 34000, 34000, 34100, 34000);")
             self.error.setText("Успешно")
+            self.close()
         except sqlite3.IntegrityError as s:
+            print(s.args)
             if s.args == "('UNIQUE constraint failed: users.login',)":
                 self.error.setText("Логин уже занят.")
-            elif s.args == ('UNIQUE constraint failed: users.mail',):
-                self.error.setText("Этот адрес электронный почты уже используется.")
+            elif s.args == ('UNIQUE constraint failed: users.email',):
+                self.error.setText("Почта уже занята.")
             else:
-                self.error.setText("Пользователь с таким логином уже зарегистрирован.")
+                self.error.setText("Логин занят.")
         self.db.connect.commit()
 
 class Personal(QWidget):
     def __init__(self, login):
         super(Personal, self).__init__()
-        self.login = login
-        loadUi('personal.ui', self)
+        self.login = login 
+        loadUi('personal.ui', self)     
         self.username.setText(login)
         self.db = StartDB()
         cursor = self.db.connect.cursor()
@@ -62,64 +69,204 @@ class Personal(QWidget):
         self.balance.setText(f"{result.fetchall()[0][0]} KGS")
         self.make.clicked.connect(self.make_money)
         self.hide_transfer()
+        self.hide_payment_buttons()
+        self.hide_payment()
         self.transfer.clicked.connect(self.user_transfer)
         self.send.clicked.connect(self.user_transfer)
-        self.db.connect.commit()
+        self.payment.clicked.connect(self.payment_1)
+        cursor.connection.commit()
+
+    def hide_buttons(self):
+        self.make.hide()
+        self.transfer.hide()
+        self.payment.hide()
+        self.history.hide()
 
     def hide_transfer(self):
-        self.login.hide()
+        self.result.hide()
+        self.input_login.hide()
         self.amount.hide()
         self.send.hide()
-        self.result.hide()
 
     def show_transfer(self):
-        self.login.show()
+        self.input_login.show()
         self.amount.show()
         self.send.show()
-        self.result.show()
+
+    def hide_payment(self):
+        self.result_2.hide()
+        self.amount_2.hide()
+        self.send_2.hide()
+
+    def hide_payment_buttons(self):
+        self.electricity.hide()
+        self.water.hide()
+        self.sewer.hide()
+        self.gas.hide()
+        self.garbage.hide()
+
+    def show_payment_buttons(self):
+        self.electricity.show()
+        self.water.show()
+        self.sewer.show()
+        self.gas.show()
+        self.garbage.show()    
+
+    def show_payment(self):
+        self.result_2.show()
+        self.amount_2.show()
+        self.send_2.show()
 
     def update_balance(self):
         cursor = self.db.connect.cursor()
         result = cursor.execute(f"SELECT balance FROM users WHERE login = '{self.login}';")
         self.balance.setText(f"{result.fetchall()[0][0]} KGS")
-        self.db.connect.commit()
     
     def make_money(self):
         cursor = self.db.connect.cursor()
         cursor.execute(f"UPDATE users SET balance = balance + {10} WHERE login = '{self.login}';")
         self.db.connect.commit()
         self.update_balance()
-        
+
     def user_transfer(self):
         self.show_transfer()
-        login = self.login.text()
+        self.result.hide()
+        input_login = self.input_login.text()
         amount = self.amount.text()
-        print(login, amount)
         cursor = self.db.connect.cursor()
-        cursor.execute(f"SELECT login FROM users WHERE login = '{login}';")
-        result = cursor.fetch()
+        cursor.execute(f"SELECT login FROM users WHERE login = '{input_login}';")
+        result = cursor.fetchall()
         if result != []:
             cursor.execute(f"SELECT balance FROM users WHERE login = '{self.login}';")
             users_balance = cursor.fetchall()[0][0]
-            
+            print(users_balance)
             if users_balance >= int(amount):
-                print('OK')
+                cursor.execute(f"UPDATE users SET balance = balance - {amount} WHERE login = '{self.login}';")
+                cursor.execute(f"UPDATE users SET balance = balance + {amount} WHERE login = '{input_login}';")
+                self.db.connect.commit()
+                self.update_balance()
+                self.result.show()
+                self.result.setText("Успешно")
             else:
-                print('Ошибка')
-        try:
-            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{mail}', '{time.ctime()}');")
-            self.error.setText("Успешно")
-        except sqlite3.IntegrityError as s:
-            print(s.args)
-            if s.args == "('UNIQUE constraint failed: users.login',)":
-                self.error.setText("Логин уже занят.")
-            elif s.args == ('UNIQUE constraint failed: users.mail',):
-                self.error.setText("Этот адрес электронный почты уже используется.")
-            else:
-                self.error.setText("Пользователь с таким логином уже зарегистрирован")
-        self.db.connect.commit()
+                self.result.show()  
+                self.result.setText("Недостаточно средств.")        
+        elif result  == [] and input_login and amount:
+            self.result.show()
+            self.result.setText("Не найден.")
 
-class Bank(QMainWindow):
+    def payment_1(self):
+        self.show_payment_buttons()
+        self.hide_buttons()
+        self.electricity.clicked.connect(self.energy)
+        self.water.clicked.connect(self.waters)
+        self.sewer.clicked.connect(self.sewers)
+        self.gas.clicked.connect(self.gass)
+        self.garbage.clicked.connect(self.garbages)
+        
+
+    def energy(self):
+        self.show_payment()
+        self.hide_payment_buttons()
+        login = self.username.text()
+        self.result_2.hide()
+        amount = self.amount_2.text()
+        print(amount)
+        
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        balance = cursor.fetchall()[0][0]
+        # if balance >= amount:
+        #     cursor.execute(f"UPDATE users SET utilities_water = utilities_water - '{amount}' WHERE login = '{login}';")
+        #     self.db.connect.commit()
+        #     self.result_2.setText("Успешно оплачено")
+        # else:
+        #     self.result_2.setText("Недостаточно средств для оплаты")
+
+    def waters(self):
+        self.show_payment()
+        self.hide_payment_buttons()
+        login = self.username.text()
+        self.result_2.hide()
+        amount = self.amount_2.text()
+        print(amount)
+        
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        balance = cursor.fetchall()[0][0]
+        # if balance >= amount:
+        #     cursor.execute(f"UPDATE users SET utilities_electricity = utilities_electricity - '{amount}' WHERE login = '{login}';")
+        #     self.db.connect.commit()
+        #     self.result_2.setText("Успешно оплачено")
+        # else:
+        #     self.result_2.setText("Недостаточно средств для оплаты")
+
+    def sewers(self):
+        self.show_payment()
+        self.hide_payment_buttons()
+        login = self.username.text()
+        self.result_2.hide()
+        amount = self.amount_2.text()
+        print(amount)
+        
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        balance = cursor.fetchall()[0][0]
+        # if balance >= amount:
+        #     cursor.execute(f"UPDATE users SET utilities_trashnyak = utilities_trashnyak - '{amount}' WHERE login = '{login}';")
+        #     self.db.connect.commit()
+        #     self.result_2.setText("Успешно оплачено")
+        # else:
+        #     self.result_2.setText("Недостаточно средств для оплаты")
+
+    def gass(self):
+        self.show_payment()
+        self.hide_payment_buttons()
+        login = self.username.text()
+        self.result_2.hide()
+        amount = self.amount_2.text()
+        print(amount)
+        
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        balance = cursor.fetchall()[0][0]
+        # if balance >= amount:
+        #     cursor.execute(f"UPDATE users SET utilities_internet = utilities_internet - '{amount}' WHERE login = '{login}';")
+        #     self.db.connect.commit()
+        #     self.result_2.setText("Успешно оплачено")
+        # else:
+        #     self.result_2.setText("Недостаточно средств для оплаты")
+
+    def garbages(self):
+        self.show_payment()
+        self.hide_payment_buttons()
+        login = self.username.text()
+        self.result_2.hide()
+        amount = self.amount_2.text()
+        print(amount)
+        
+        cursor = self.db.connect.cursor()
+        cursor.execute(f"SELECT balance FROM users WHERE login = '{login}';")
+        balance = cursor.fetchall()[0][0]
+        # if balance >= amount:
+        #     cursor.execute(f"UPDATE users SET utilities_gas = utilities_gas - '{amount}' WHERE login = '{login}';")
+        #     self.db.connect.commit()
+        #     self.result_2.setText("Успешно оплачено")
+        # else:
+        #     self.result_2.setText("Недостаточно средств для оплаты")
+
+
+
+    # def payments(self):
+    #     self.show_payment_buttons()
+    #     self.result_2.hide()
+    #     amount = self.amount_2.text()
+    #     if 
+    #     cursor = self.db.connect.cursor()
+        
+    #     cursor.execute(f"UPDATE users SET communalka_water = communalka_water - {amount} WHERE login = '{self.login_2}';")
+        
+
+class Bank(QMainWindow, ):
     def __init__(self):
         super(Bank, self).__init__()
         loadUi('main.ui', self)
@@ -146,12 +293,14 @@ class Bank(QMainWindow):
         result = cursor.fetchall()
         if result != []:
             self.show_error()
-            self.error.setText('Ok')
+            self.error.setText("Ok")
             self.personal = Personal(login)
             self.personal.show()
+            self.close()
         else:
             self.show_error()
             self.error.setText("Неправильные данные")
+
 
 app = QApplication(sys.argv)
 bank = Bank()
